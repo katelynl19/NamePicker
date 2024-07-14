8a03101903bd6277659be319096934ca36e6892d
katelynl19 <katelynliu@rocknrollrobots25.com>
2024-07-14 11:43:56 -0700

    Initial Commit

---
+++ NamePicker/NamePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamePickerApp.swift
+//  NamePicker
+//
+//  Created by Katelyn Liu on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NamePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
