21f1e608dc78bb5f41d01442ec8e632a188280c6
katelynl19 <katelynliu@rocknrollrobots25.com>
2024-07-14 16:52:19 -0700

    made the random name picker with a text field and button

--- NamePicker/ContentView.swift
+++ NamePicker/ContentView.swift
@@ -8,12 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var names: [String] = []
+    @State private var nameToAdd = ""
+    @State private var pickedName = ""
+    @State private var shouldRemovePickedName = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack(spacing: 8) {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Random Name Picker")
+            }
+            .font(.title)
+            .bold()
+            // ternary conditional operator: if pickedName is empty, shows an empty line on the page. if pickedName has name, it displays the name (TRUE : FALSE)
+            Text(pickedName.isEmpty ? " " : pickedName)
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.tint)
+            
+            List {
+                ForEach(names, id: \.self) { name in
+                    Text(name)
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            TextField("Add Name", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            
+            Divider()
+            
+            Toggle("Remove when picked", isOn: $shouldRemovePickedName)
+            
+            Button {
+                // if let allows us to work with the possibility of nil values (array is empty) - if a random name is chosen from array, it runs first block. if not, it runs the code under "else"
+                if let randomName = names.randomElement() {
+                    pickedName = randomName
+                    
+                    // if user wants to remove picked names, remove the name in names that equals the random name
+                    if shouldRemovePickedName {
+                        names.removeAll { name in
+                            return (name == randomName)
+                        }
+                    }
+                }
+                else {
+                    pickedName = ""
+                }
+            } label: {
+                Text("Pick Random Name")
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 16)
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            
+            
         }
         .padding()
     }
